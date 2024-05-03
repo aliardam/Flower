3805a398ec5dbff6440e730453b6f22d479a00b9
Ali <aliardam402@gmail.com>
2024-05-03 22:44:10 +0200

    adding some stuff

--- Flowers.cpp
+++ Flowers.cpp
@@ -2,15 +2,33 @@
 #include <cassert>
 using namespace std;
 
+class flower{
+    int fertneed;
+    bool fertilized=false;
+    int waterneed;
+    bool watered=false;
+    bool condition=false;
+    public :
+    void water(int &wateramount){
+        if (waterneed<=wateramount)
+        {
+            wateramount -=waterneed;
+            cout<<"your flower has been watered";
+            watered=true;
+        }
+    } 
+};
+
+
 // my humble tests
 void TEST ()
 {
     flower* testflower = new rose();
     int wateramount = 0;
     int fertamount = 20;
-    testflower->fertilize();
-    testflower->water();
-    bool con = testflower->checkcondition;
+    testflower->fertilize(fertamount);
+    testflower->water(wateramount);
+    bool con = testflower->checkcondition();
     assert(!con);
 }
 
