1b66e43d4b551a228e199f0825910304372953c7
Ali <aliardam402@gmail.com>
2024-05-11 16:35:58 +0200

    im such an idiot

--- Flowers.cpp
+++ Flowers.cpp
@@ -3,93 +3,6 @@
 #include <vector>
 using namespace std;
 string decapitalize(const string& str);
-//my base class
-class flower{
-    int fertneed;
-    bool fertilized=false;
-    int waterneed;
-    bool watered=false;
-    bool condition=false;
-    public :
-    void water(int &wateramount){
-        if (waterneed<=wateramount)
-        {
-            wateramount -=waterneed;
-            cout<<"Congrats! your flower has been watered !! ";
-            watered=true;
-        }
-        else{
-            cout<<"not enough water :,(";
-        }
-    } 
-    void fertilize (int &fertamount){
-              if (fertneed<=fertamount)
-        {
-            fertamount-=fertneed;
-            cout<<"Congrats! your flower has been fertilized !! ";
-            fertilized=true;
-        }
-        else{
-            cout<<"not enough fertilizer  :,(";
-        }
-    } 
-    bool checkcondition(){
-        if (watered || fertilized)
-        condition=true;
-        else
-        condition=false;
-        return condition;
-    }
-    virtual ~flower() {}
-    };
-
-    class rose : public flower {
-        string color;
-        int fertneed=10;
-        int waterneed=15;
-        public:
-        void setcolor(string colorname)
-        {
-            string red, pink, white, yellow, orange, peach, purple; 
-            vector<string> possiblecolors = {red, pink, white, yellow, orange, peach, purple };
-            colorname = decapitalize(colorname);
-            for (int i=0;i<possiblecolors.size();i +=1){
-                if (colorname==possiblecolors[i]){
-                    color=colorname;
-                }
-                else{
-                    cout<<"input invalid, or the color is not a natural accuring color of this flower. "<<endl;
-                }
-            }
-        }
-        string const getcolor() const {
-            return color;
-        }
-
-    };
-
-
-
-// my humble tests
-void TEST ()
-{
-    flower* testflower = new rose();
-    int wateramount = 0;
-    int fertamount = 20;
-    testflower->setcolor(Red);
-    testflower->fertilize(fertamount);
-    testflower->water(wateramount);
-    bool con = testflower->checkcondition();
-    assert(!con);
-    assert(testflower->getcolor()==red);
-}
-
-
-int main()
-{
-TEST();
-return 0;
-}
 //ngl i found this online
 string decapitalize(const string& str) {
     string result;
@@ -103,4 +16,92 @@ string decapitalize(const string& str) {
     }
 
     return result;
+}
+//my base class
+class flower{
+    int fertneed;
+    bool fertilized=false;
+    int waterneed;
+    bool watered=false;
+    bool condition=false;
+    public :
+    void water(int &wateramount){
+        if (waterneed<=wateramount)
+        {
+            wateramount -=waterneed;
+            cout<<"Congrats! your flower has been watered !! "<<endl;
+            watered=true;
+        }
+        else{
+            cout<<"not enough water :,("<<endl;
+        }
+    } 
+    void fertilize (int &fertamount){
+              if (fertneed<=fertamount)
+        {
+            fertamount-=fertneed;
+            cout<<"Congrats! your flower has been fertilized !! "<<endl;
+            fertilized=true;
+        }
+        else{
+            cout<<"not enough fertilizer  :,("<<endl;
+        }
+    } 
+    bool checkcondition(){
+        if (watered || fertilized)
+        condition=true;
+        else
+        condition=false;
+        return condition;
+    }
+    virtual void setcolor (const string& colorname) = 0;
+    virtual string const getcolor() = 0;
+    virtual ~flower() {}
+    };
+
+    class rose : public flower {
+        string color;
+        int fertneed=10;
+        int waterneed=15;
+        public:
+        void setcolor (const string& colorname) override
+        {
+            vector<string> possiblecolors = {"red", "pink", "white","yellow","orange","peach","purple"};
+            string colorname_;
+            colorname_ = decapitalize(colorname);
+            for (int i=0;i<possiblecolors.size();i +=1){
+                if (colorname_ == possiblecolors[i]) {
+                color = colorname_;
+                return;
+            }
+            }
+             cout << "input invalid, or the color is not a natural accuring color of this flower. " << endl;
+        color = "unknown"; // set default color
+        }
+        string const getcolor() override {
+            return color;
+        }
+
+    };
+
+// my humble tests
+void TEST ()
+{
+    flower* testflower = new rose();
+    int wateramount = 0;
+    int fertamount = 20;
+    string colorname = "Red";
+    testflower->setcolor(colorname);
+    testflower->fertilize(fertamount);
+    testflower->water(wateramount);
+    bool con = testflower->checkcondition();
+    assert(!con);
+    assert(testflower->getcolor()=="red");
+}
+
+
+int main()
+{
+TEST();
+return 0;
 }
