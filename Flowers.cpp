1c8763b17015db338be88d176492f18be2390a34
Ali <aliardam402@gmail.com>
2024-05-30 01:35:07 +0200

    ah yes , its all coming together.

--- Flowers.cpp
+++ Flowers.cpp
@@ -17,11 +17,11 @@ string decapitalize(const string& str) {
     return result;
 }
 class flower{
+    int amount=1;
     protected:
     vector<string> possiblecolors;
     string color;
     double pricepp;
-    int amount;
  
     public :
     void setcolor (string& colorname)
@@ -40,6 +40,14 @@ class flower{
     string const getcolor() { return color;}
     double const getprice(){
     return amount * pricepp;
+}
+void setamount(int _am){
+    if (_am<20 && _am>0){
+        amount=_am;
+    }
+    else{
+        throw invalid_argument("flower amount is an invalid number.");
+    }
 }
     virtual ~flower() {}
     };
@@ -72,10 +80,9 @@ double const getFullPrice (){
 };
 class decoration {
 protected:
-int amount;
+int amount=1;
 int amountlimit;
 double pricepp; //price per piece
-
 public:
 void setamnt(int amnt){
     if (amountlimit>=amnt && amnt>0){
@@ -89,34 +96,132 @@ double const getprice(){
     return amount * pricepp;
 }
 };
+class ribbon : public decoration {
+    string color="white";
+    vector<string> possiblecolors= {"white","black","green","blue","red","yellow","gray","orange","pink"};
+    public:
+    ribbon()
+    {
+        amountlimit=5;
+        pricepp=0.75;
+    }
+    ribbon(string _color)
+    {
+        amountlimit=5;
+        pricepp=0.75;
+        setcolor(_color);
+    }
+        ribbon(string _color,int am)
+    {
+        amountlimit=5;
+        pricepp=0.75;
+        setcolor(_color);
+        setamnt(am);
+    }
+    void setcolor (string& colorname)
+    {
+        string colorname_;
+        colorname_ = decapitalize(colorname);
+        for (int i=0;i<possiblecolors.size();i +=1){
+            if (colorname_ == possiblecolors[i]) {
+            color = colorname_;
+            return;
+        }
+        }
+         cout << "input invalid, or the color is not currantly available in our shop " << endl;
+    }
+    string const getcolor(){return color;}
+};
+class Wrappingpaper : public decoration {
+    string color="white";
+    vector<string> possiblecolors= {"white","black","green","blue","red","yellow","gray","orange","pink"};
+    public:
+    Wrappingpaper()
+    {
+        amountlimit=6;
+        pricepp=0.5;
+    }
+    Wrappingpaper(string _color)
+    {
+        amountlimit=6;
+        pricepp=0.5;
+        setcolor(_color);
+    }
+    Wrappingpaper(string _color,int am)
+    {
+        amountlimit=6;
+        pricepp=0.5;
+        setcolor(_color);
+        setamnt(am);
+    }
+    void setcolor (string& colorname)
+    {
+        string colorname_;
+        colorname_ = decapitalize(colorname);
+        for (int i=0;i<possiblecolors.size();i +=1){
+            if (colorname_ == possiblecolors[i]) {
+            color = colorname_;
+            return;
+        }
+        }
+         cout << "input invalid, or the color is not currantly available in our shop " << endl;
+    }
+    string const getcolor(){return color;}
+};
 class daisy : public flower {
         public:
         daisy() {
+        pricepp=1.50;
         possiblecolors = {"white", "pink", "yellow", "red", "lavender", "purple"};
     };
         daisy(string _color){
+        pricepp=1.50;
         possiblecolors = {"white", "pink", "yellow", "red", "lavender", "purple"};
         setcolor(_color);
 }
-};
-class sage : public flower {
-    public:
-    sage() {
-    possiblecolors = {"blue", "purple", "red", "pink", "white", "yellow", "orange"};
-    };
-    sage(string _color){
-        possiblecolors = {"blue", "purple", "red", "pink", "white", "yellow", "orange"};
+    daisy(string _color,int _am){
+        pricepp=1.50;
+        possiblecolors =  {"white", "pink", "yellow", "red", "lavender", "purple"};
         setcolor(_color);
+        setamount(_am);
+    }
+};
+class sunflower : public flower {
+    public:
+    sunflower() {
+    pricepp=2.50;
+    possiblecolors = {"yellow", "red", "orange", "mahogany", "white"};
+    };
+    sunflower(string _color){
+        pricepp=2.50;
+        possiblecolors = {"yellow", "red", "orange", "mahogany", "white"};
+        setcolor(_color);
+    }
+    sunflower(string _color,int _am){
+        pricepp=2.50;
+        possiblecolors = {"yellow", "red", "orange", "mahogany", "white"};
+        setcolor(_color);
+        setamount(_am);
     }
 };
  class rose : public flower {
         public:
+        
         rose() {
-        possiblecolors = {"red", "pink", "white","yellow","orange","peach","purple"};    };
+        pricepp=3.75;
+        possiblecolors = {"red", "pink", "white","yellow","orange","peach","purple"};
+        }
         rose(string _color){
+            pricepp=3.75;
              possiblecolors = {"red", "pink", "white","yellow","orange","peach","purple"};
             setcolor(_color);
             }; 
+       rose(string _color,int _am){
+        pricepp=3.75;
+             possiblecolors = {"red", "pink", "white","yellow","orange","peach","purple"};
+            setcolor(_color);
+            setamount(_am);
+            };
     };
 // my humble tests
 void testRoseColor() {
